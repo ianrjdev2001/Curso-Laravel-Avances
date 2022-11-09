8c18807f10300c5c4b46bd20f93dc8982a6a07f4
Ian Manuel <ianmanuelrj@hotmail.com>
2022-11-09 11:56:21 -0700

    Enviar emails

--- my-blog/resources/views/layouts/plantilla.blade.php
+++ my-blog/resources/views/layouts/plantilla.blade.php
@@ -5,22 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .active
+        {
+            color: red;
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
     <!--header-->
-    <header>
-        <h1>Coders Free</h1>
-        <nav>
-            <ul>
-                <li><a href="{{route('home')}}">Home</a></li>
-                <li><a href="{{route('cursos.index')}}">Cursos</a></li>
-                <li><a href="{{route('nosotros')}}">Nosotros</a></li>
-            </ul>
-        </nav>
-    </header>
+    @include('layouts.partials.header')
     <!--nav-->
     @yield('content')
     <!--footer-->
+    @include('layouts.partials.footer')
     <!--script-->
 </body>
 </html>
