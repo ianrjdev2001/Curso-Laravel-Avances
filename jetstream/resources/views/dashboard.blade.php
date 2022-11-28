5c162a2d5f01343fa4cfe1bb9f584d46625b5f34
Ian Manuel <ianmanuelrj@hotmail.com>
2022-11-28 11:17:01 -0700

    reutilizar plantlilla jetsrtream

--- jetstream/resources/views/dashboard.blade.php
+++ jetstream/resources/views/dashboard.blade.php
@@ -1,14 +1,17 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Laravel
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-        <x-jet-welcome />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio velit modi numquam possimus corporis debitis quos qui hic cupiditate tempora eligendi, ea alias soluta! Quibusdam, voluptatum! Repellendus sapiente necessitatibus velit?</p>
+            <p>Eum tempora aspernatur impedit. Eveniet qui exercitationem, obcaecati illo eaque sit assumenda quia expedita totam corporis aut odit! Quidem veniam laborum voluptatum tempora nemo tempore quia placeat recusandae ipsam ea.</p>
+            <p>Delectus perferendis dolor commodi harum enim fugiat quis at alias ipsam nobis facilis, perspiciatis, quidem tempore labore iusto consectetur vitae! Nam consequatur sequi veritatis, repellendus itaque minus ullam aut vel?</p>
+            <p>Vero consectetur repellat eum ipsam odit, voluptates laudantium possimus fuga molestias dolorum, soluta laborum eligendi? Illum dignissimos dolorem, ex laudantium explicabo nostrum neque consequuntur, nam perferendis rem, velit voluptatibus laboriosam?</p>
+            <p>Quia, voluptatibus officia. Ex enim sint natus! Facilis odio earum reiciendis ratione expedita vel est vitae assumenda! Provident ipsum labore aperiam iste. Dolores reprehenderit, ad inventore quisquam facilis non? Dolores.</p>
         </div>
     </div>
 </x-app-layout>
